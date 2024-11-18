71947c3620bf8de23ecba153fc3c1047f4c85e67
VishveshRoshan <164897739+VishveshRoshan@users.noreply.github.com>
2024-11-18 12:56:44 +0530

    Add files via upload

---
+++ text.js
@@ -0,0 +1,27 @@
+const display = document.getElementById('display');
+
+function appendNumber(number) {
+  display.value += number;
+}
+
+function appendOperator(operator) {
+  if (display.value) {
+    display.value += operator;
+  }
+}
+
+function clearDisplay() {
+  display.value = '';
+}
+
+function deleteLast() {
+  display.value = display.value.slice(0, -1);
+}
+
+function calculateResult() {
+  try {
+    display.value = eval(display.value);
+  } catch {
+    display.value = 'Error';
+  }
+}
